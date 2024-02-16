f411911172a9e636c96f0bd7504d1d236521ba11
patrikx3 <alabard@gmail.com>
2024-02-16 12:27:11 +0100

    r0b08x [chore]   2/16/2024, 12:27:11 PM

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,7 @@
 module.exports = (grunt) => {
     const builder = require(`corifeus-builder`);
     const loader = new builder.loader(grunt);
-    loader.js({
-        replacer: {
-            npmio: true
-        }
-    });
+    loader.js({});
 
     grunt.registerTask('default', builder.config.task.build.js);
 
