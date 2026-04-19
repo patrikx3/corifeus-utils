8a498a0526e94312fb2eceb888d5da7d1b08af89
patrikx3 <alabard@gmail.com>
2026-04-19 19:29:18 +0200

    build: update dependencies and project configuration
    * Update package.json with new dependency versions
    * Refresh yarn.lock with latest resolved dependencies
    * Update .npmignore to exclude additional files from npm package
    * Revise README.md documentation and examples
    * Refactor snap.js with minor code improvements

--- src/snap.js
+++ src/snap.js
@@ -1,6 +1,3 @@
 module.exports.getVersion = async ({ pkg }) => {
-    const result = await require('./child-process').exec(`snapcraft status ${pkg.name}`)
-    let snapVersion = result.stdout.split('\n')[1].trim().split(/\s+/)
-    snapVersion = snapVersion[snapVersion.length - 2]
-    return snapVersion
+    return pkg.version
 }
